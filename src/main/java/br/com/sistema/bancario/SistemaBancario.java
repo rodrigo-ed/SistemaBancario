5c352171fe835ad5847bad5117f42a529a6fa30e
Rodrigo Eduardo <eduardorodrigoalmeida18@gmail.com>
2025-09-23 16:02:58 -0300

    first commit

---
+++ src/main/java/br/com/sistema/bancario/SistemaBancario.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.sistema.bancario;
+
+/**
+ *
+ * @author eduar
+ */
+public class SistemaBancario {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
